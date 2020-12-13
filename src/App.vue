653b0369d69306194852c6fa10f863456cbee6cd
saslavik <saslavik@gmail.com>
2020-12-13 17:44:03 +0200

    scss added

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+  <div class="wrapper">
+    <!-- header -->
+    <div class="wrapper-content">
+      <section>
+        <div class="container">
+          <div id="app">
+            <h1>HELLO</h1>
+            <img alt="Vue logo" src="./assets/logo.png">
+          </div>
+        </div>
+      </section>
+    </div>
+    <!-- footer -->
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+
+import '@/assets/scss/main.scss';
 
 Vue.config.productionTip = false
 
